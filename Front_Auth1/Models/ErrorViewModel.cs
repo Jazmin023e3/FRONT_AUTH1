d1c6a6eb510dcdc54da045daab7325d3c537acf4
Jazmin Aguilar <Jazmin086441@outlook.es>
2025-09-25 20:47:01 -0600

    Inicio

---
+++ Front_Auth1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Front_Auth1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
