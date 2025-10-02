261c73422433f9fd5643c3856b9eb78d66434858
Jazmin Aguilar <Jazmin086441@outlook.es>
2025-10-01 20:36:24 -0600

    se quito el logic

---
+++ Front_Auth1/Models/Producto.cs
@@ -0,0 +1,10 @@
+﻿namespace Front_Auth1.Models
+{
+    public class Producto
+    {
+        public string Nombre { get; internal set; }
+        public int Id { get; internal set; }
+        public int Stock { get; internal set; }
+        public decimal Precio { get; internal set; }
+    }
+}
